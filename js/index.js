0fd05e34a89d98a96737d86eda84a1f24741f6cf
Gereon Czerny <gereon.czerny@yahoo.com>
2023-03-17 13:10:45 +0100

    finalpushithink

--- js/index.js
+++ js/index.js
@@ -16,6 +16,8 @@
   dogStandImg.src = "images/doggo2.png";
   const dogJumpImg = new Image();
   dogJumpImg.src = "images/doggojump.png";
+  const obstacleImg = new Image();
+  obstacleImg.src = "images/asteroids.png";
 
   let dogImg = dogStandImg;
   let counter = 0;
@@ -47,7 +49,7 @@
             clearInterval(descendId);
             isJumping = false;
           }
-        }, 20);
+        }, 35);
       }
     }, 10);
   }
@@ -69,73 +71,113 @@
   });
 
 
-
   function startGame() {
 
     //hide button
     document.getElementById("start-button").style.display = "none";
+    document.getElementById("cover").style.display = "none";
+  
+
+    const bgMusic = document.getElementById("bg-music");
+    bgMusic.play()
 
     //set up basics
     ctx.drawImage(img, 0, 0, 1800, 900);
     drawDog();
-
+  
     //set up game counter
     function updateCounter() {
       counter++;
     }
     setInterval(updateCounter, 1000);
-
+    
+    let gameOverFlag = false;
+  
     //obstacles and stages
-      function updateObstacles() {
+    function updateObstacles() {
+      if (!gameOverFlag) {
         ctx.clearRect(0, 0, theCanvas.width, theCanvas.height);
-        if (counter < 10){
+  
+        // update background image based on game counter
+        if (counter < 10) {
           img = img;
-        }
-        else if (counter > 10 && counter < 20){
+        } else if (counter < 20) {
           img = img2;
-        }
-        else if (counter > 20 && counter < 30){
+        } else if (counter < 30) {
           img = img3;
-        }
-        else if (counter > 30){
+        } else {
           img = img4;
         }
+  
         ctx.drawImage(img, 0, 0, 1800, 900);
+  
+        // draw dog
         if (isJumping) {
           dogImg = dogJumpImg;
         } else {
           dogImg = dogStandImg;
         }
         ctx.drawImage(dogImg, dogX, dogY, 350, 300);
-        ctx.fillStyle = 'white';
-        obstacles.forEach((obstacle) => {
-          obstacle.x -= 5;
-          ctx.fillRect(obstacle.x, obstacle.y, obstacle.width, obstacle.height);
-          if (dogX + 300 > obstacle.x && dogX < obstacle.x + obstacle.width && dogY + 200 > obstacle.y && dogY < obstacle.y + obstacle.height) {
-            gameOver();
-          }
-        });
-        requestAnimationFrame(updateObstacles);
+  
+    // draw obstacles
+    obstacles.forEach((obstacle) => {
+      obstacle.x -= 3;
+      const obstacleImg = new Image();
+      obstacleImg.src = "images/asteroids.png";
+      ctx.drawImage(obstacleImg, obstacle.x, obstacle.y, obstacle.width, obstacle.height);
+      if (dogX + 300 > obstacle.x && dogX < obstacle.x + obstacle.width && dogY + 200 > obstacle.y && dogY < obstacle.y + obstacle.height) {
+        gameOver();
       }
-      requestAnimationFrame(updateObstacles);
+    });
 
-      //actually spawn the obstacles
-      function spawnObstacle() {
-        const maxWidth = 200;
-        const minWidth = 50;
-        const maxObstY = 800;
-        const minObstY = 400;
-        const obstacleY = Math.floor(Math.random() * (maxObstY - minObstY + 1)) + minObstY;
-        const obstacleWidth = Math.floor(Math.random() * (maxWidth - minWidth + 1)) + minWidth;
-        obstacles.push({
-          x: theCanvas.width, // spawn on right edge of canvas
-          y: obstacleY,
-          width: obstacleWidth,
-          height: 50,
-        });
-      }      
-      setInterval(spawnObstacle, 1500);
+    // remove obstacles that have gone off-screen
+    obstacles = obstacles.filter(obstacle => obstacle.x > -obstacle.width);
+
+    if (obstacles.length === 0) {
+      const obstacleWidth = 100;
+      const obstacleY = Math.floor(Math.random() * (theCanvas.height - 100 - jumpHeight)) + 50;
+      obstacles.push({
+        x: theCanvas.width,
+        y: obstacleY,
+        width: obstacleWidth,
+        height: obstacleWidth,
+      });
     }
   
-    document.getElementById("start-button").onclick = startGame;
+        if (!gameOverFlag) {
+          requestAnimationFrame(updateObstacles);
+        }
+      }
+    }
+    requestAnimationFrame(updateObstacles);
+  
+    // handle game over
+    function gameOver() {
+      // load game over image
+      const gameOverImg = new Image();
+      gameOverImg.src = 'images/gameover.png';
+    
+      // create an Audio object for the audio clip
+      const gameOverAudio = new Audio('images/missionfailed.mp3');
+    
+      // wait for the image to load
+      gameOverImg.onload = () => {
+        // display the image
+        ctx.drawImage(gameOverImg, 0, 0, theCanvas.width, theCanvas.height);
+        // play the audio clip
+        gameOverAudio.play();
+      };
+    
+      // reset game state
+      gameOverFlag = true;
+      clearInterval(obstacleIntervalId);
+      location.reload();
+      document.getElementById("start-button").style.display = "block";
+    }
+    
+  
+  }
+  
+  document.getElementById("start-button").onclick = startGame;
+  
   })();
